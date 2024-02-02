96948ebf7b5dd533044e414ba5c601b8b7f892cb
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-01 21:50:23 -0500

    feat: add question options dynamically with map and spread operator

--- lib/screens/questions_screen.dart
+++ lib/screens/questions_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+
 import 'package:quizzy/widgets/option_button.dart';
+import 'package:quizzy/data/questions.dart';
 
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
@@ -11,35 +13,24 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
+    final currentQuestion = questions[2];
     return SizedBox(
       width: double.infinity,
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          const Text(
-            "The Question",
-            style: TextStyle(
+          Text(
+            currentQuestion.text,
+            style: const TextStyle(
               color: Colors.white,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
             ),
           ),
           const SizedBox(height: 30),
-          OptionButton(
-            label: "Option 1",
-            handler: () {},
-          ),
-          OptionButton(
-            label: "Option 1",
-            handler: () {},
-          ),
-          OptionButton(
-            label: "Option 1",
-            handler: () {},
-          ),
-          OptionButton(
-            label: "Option 1",
-            handler: () {},
+          ...currentQuestion.options.map(
+            (option) => OptionButton(
+              label: option,
+              handler: () {},
+            ),
           ),
         ],
       ),

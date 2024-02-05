74a337a3959117f960686927d433384650a6484a
Edwin Cardenas <eacardenase@unal.edu.co>
2024-02-04 20:04:22 -0500

    feat: make answers column scrollable to prevent overflow

--- lib/widgets/questions_summary.dart
+++ lib/widgets/questions_summary.dart
@@ -10,29 +10,34 @@ class QuestionsSummary extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: summaryData.map(
-        (summary) {
-          return Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "${(summary['questionIndex'] as int) + 1}",
-              ),
-              Expanded(
-                child: Column(
-                  children: [
-                    Text(summary["question"] as String),
-                    const SizedBox(height: 5),
-                    Text(summary["selectedAnswer"] as String),
-                    Text(summary["correctAnswer"] as String)
-                  ],
-                ),
-              )
-            ],
-          );
-        },
-      ).toList(),
+    return SizedBox(
+      height: 300,
+      child: SingleChildScrollView(
+        child: Column(
+          children: summaryData.map(
+            (summary) {
+              return Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "${(summary['questionIndex'] as int) + 1}",
+                  ),
+                  Expanded(
+                    child: Column(
+                      children: [
+                        Text(summary["question"] as String),
+                        const SizedBox(height: 5),
+                        Text(summary["selectedAnswer"] as String),
+                        Text(summary["correctAnswer"] as String)
+                      ],
+                    ),
+                  )
+                ],
+              );
+            },
+          ).toList(),
+        ),
+      ),
     );
   }
 }
